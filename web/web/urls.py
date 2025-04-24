c3352613570e088033e29ee55e9034ed829d3054
dgush <dgushch@gmail.com>
2025-04-24 12:58:06 +0400

    alpha 0.2.16

--- web/web/urls.py
+++ web/web/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/", include('api_1.urls')),
+    path("api/v1/", include('bot_admin.urls')),
 ]
